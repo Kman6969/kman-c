cdecb23754b78383b8b4b31e0f1b246799ad89cc
Kman6969 <55087020+Kman6969@users.noreply.github.com>
2019-09-09 20:18:41 +0530

    Add files via upload

---
+++ for loop/power.c
@@ -0,0 +1,17 @@
+//finding power without function
+
+#include<stdio.h>
+int main()
+{
+	int x,n,i,pow=1;
+	printf("\nEnter base x and power n : ");
+	scanf("%d%d",&x,&n);
+	if(n==0)
+		printf("\npower is 1\n");
+	else
+	{
+	for(i=1;i<=n;i++)
+		pow=pow*x;
+	printf("\npower is : %d",pow);
+	}
+}
