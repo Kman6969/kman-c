38b3c81d475d680cacb4e7e4f2bbfa16b7cd2ae6
Kman6969 <55087020+Kman6969@users.noreply.github.com>
2019-09-09 22:19:10 +0530

    Add files via upload

---
+++ while loop/OnebyOne.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,rev=0;
+	printf("\nEnter a number : ");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		rev=rev*10+n%10;
+		n=n/10;
+	}
+	printf("%d",rev);
+	i=rev;
+	while(rev!=0)
+	{
+		i=rev%10;
+		
+		rev=rev/10;
+	printf(" %d ",i);
+	}	
+	return 0;
+
+}
